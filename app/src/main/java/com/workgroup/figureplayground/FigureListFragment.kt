2580d56d55a50d92bdc4d6c1c7efa4c4e8fa0dcc
Lukasz Matysik <lukasz.matysik6@gmail.com>
2019-04-30 14:10:46 +0200

    Main menu functionality handled

--- app/src/main/java/com/workgroup/figureplayground/FigureListFragment.kt
+++ app/src/main/java/com/workgroup/figureplayground/FigureListFragment.kt
@@ -12,20 +12,33 @@ import kotlinx.android.synthetic.main.fragment_figure_list.view.*
 
 class FigureListFragment : Fragment() {
 
+    var mMode = 0
+    var itemList = arrayOf(FigureListItem(R.drawable.flat_fig, "Triangle"))
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_figure_list, container, false)
 
-        val listItems = arrayOf(FigureListItem(R.drawable.flat_fig, "Triangle"),FigureListItem(R.drawable.flat_fig, "Square")
-            ,FigureListItem(R.drawable.flat_fig, "Item 3"))
+        if(mMode == 0){
+            itemList = arrayOf(FigureListItem(R.drawable.flat_fig, "Triangle"),FigureListItem(R.drawable.flat_fig, "Square")
+                ,FigureListItem(R.drawable.flat_fig, "Rectangle"),FigureListItem(R.drawable.flat_fig, "Diamond"),FigureListItem(R.drawable.flat_fig, "Circle"))
+        }
+        else{
+            itemList = arrayOf(FigureListItem(R.drawable.flat_fig, "Cube"),FigureListItem(R.drawable.flat_fig, "Sphere")
+                ,FigureListItem(R.drawable.flat_fig, "Cone"),FigureListItem(R.drawable.flat_fig, "cuboid"),FigureListItem(R.drawable.flat_fig, "somhing"))
+        }
 
         var listView = view.FlatFiguresList
 
-        var arrayAdapter = FigureListAdapter(context!!,R.layout.adapter_view_layout, listItems)
+        var arrayAdapter = FigureListAdapter(context!!,R.layout.adapter_view_layout, itemList)
         listView.adapter = arrayAdapter
 
         return view
     }
+
+    public fun setMode( mode: Int){
+        this.mMode = mode
+    }
+
 }
--- app/src/main/java/com/workgroup/figureplayground/MainActivity.kt
+++ app/src/main/java/com/workgroup/figureplayground/MainActivity.kt
@@ -2,11 +2,34 @@ package com.workgroup.figureplayground
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),MainMenuFragment.MainMenuFragmentListener {
+
+
+    private val manager: FragmentManager = supportFragmentManager
+    private val mainMenuFragment = MainMenuFragment()
+
+
+    override fun showFiguresListView(mode: Int) {
+        val figureListFragment = FigureListFragment()
+        figureListFragment.mMode = mode
+        val transaction = manager.beginTransaction()
+        transaction.add(R.id.main_frame, figureListFragment)
+        transaction.replace(R.id.main_frame, figureListFragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val transaction = manager.beginTransaction()
+        transaction.add(R.id.main_frame, mainMenuFragment)
+        transaction.replace(R.id.main_frame, mainMenuFragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
